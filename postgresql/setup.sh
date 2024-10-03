ad412989e9a0a8192b4bf6c17f45df297f286bbc
nusbru <nusbru@protonmail.com>
2024-10-03 16:15:25 +0100

    Update setup.sh
    change variables values.

--- postgresql/setup.sh
+++ postgresql/setup.sh
@@ -4,9 +4,9 @@
 
 echo "Iniciando o script de configuração do PostgreSQL..."
 
-dbname="meubanco"
-dbuser="meuusuario"
-dbpassword="minhasenha"
+dbname="curso_docker"
+dbuser="docker_usr"
+dbpassword="docker_pwd"
 
 docker run -d --name postgresql -e POSTGRES_DB=$dbname -e POSTGRES_USER=$dbuser -e POSTGRES_PASSWORD=$dbpassword -p 5432:5432 postgres:latest
 
